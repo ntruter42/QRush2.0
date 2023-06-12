4c930d8094946a6292dde69d130ae3a3f29444d4
ntruter42 <ntruter42@gmail.com>
2023-06-12 11:18:43 +0200

    Add index.html, style.css, qrush.map.js - created basic layout with leaflet map api, buttons for links and minimal prize display

--- js/qrush.map.js
+++ js/qrush.map.js
@@ -1,11 +1,12 @@
 let latitude = -33.92835836470535;
 let longitude = 18.423887682529877;
-let zoomLevel = 10;
+let zoomLevel = 15;
 
 function drawMap(lat, lon, zoom) {
 	// create map object
+	// let map = L.map('map').locate({setView: true, maxZoom: 16});
 	let map = L.map('map').setView([lat, lon], zoom);
-
+	
 	// draw map
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
@@ -19,3 +20,22 @@ function addMarker(mLat, mLon, map) {
 	let marker = L.marker([mLat, mLon]).addTo(map);
 }
 
+let map = drawMap(latitude, longitude, zoomLevel);
+
+// map.locate({ setView: true, maxZoom: 16 });
+// function onLocationFound(e) {
+// 	var radius = e.accuracy;
+
+// 	L.marker(e.latlng).addTo(map)
+// 		.bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+// 	L.circle(e.latlng, radius).addTo(map);
+// }
+
+// map.on('locationfound', onLocationFound);
+
+// function onLocationError(e) {
+// 	alert(e.message);
+// }
+
+// map.on('locationerror', onLocationError);
